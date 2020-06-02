7fc74960b9b2d37245999963f603d64fabb119f4
Ntsika <thentsikamabe@gmail.com>
2020-06-02 20:05:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/helpinghand/CreateAccount.java
@@ -0,0 +1,95 @@
+package com.example.helpinghand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class CreateAccount extends AppCompatActivity {
+
+    public void processJSON(String json){
+        try {
+            JSONArray all = new JSONArray(json);
+            for (int i=0; i<all.length(); i++){
+                JSONObject item = all.getJSONObject(i);
+                String name = item.getString("BRAND");
+                String description = item.getString("NUMBER");
+                TextView text = new TextView(this);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doCreateAccount(View view){
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://lamp.ms.wits.ac.za/home/s2241186/signup.php")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    final String myResponse = response.body().string();
+
+                    CreateAccount.this.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void doLoginIntent(View view){
+        Intent loginIntent = new Intent(CreateAccount.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+}
---
+++ app/src/main/java/com/example/helpinghand/PatientHomePage.java
@@ -0,0 +1,14 @@
+package com.example.helpinghand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_home_page);
+    }
+}
