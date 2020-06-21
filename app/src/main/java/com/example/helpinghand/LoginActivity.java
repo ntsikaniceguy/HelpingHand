cbc574e54538fb904bada1d787cf6bed315452a3
Ntsika <thentsikamabe@gmail.com>
2020-06-21 17:24:18 +0200

    Merge branch 'master' of github.com:/ntsikaniceguy/HelpingHand
    # Conflicts:
    #	app/src/main/java/com/example/helpinghand/CreateAccount.java
    #	app/src/main/java/com/example/helpinghand/LoginActivity.java
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/com/example/helpinghand/LoginActivity.java
+++ app/src/main/java/com/example/helpinghand/LoginActivity.java
@@ -41,7 +41,7 @@ public class LoginActivity extends AppCompatActivity {
 
     void LoginToPatient(String json)
     {
-        Intent patientHomePage = new Intent(this, PatientMapActivity.class);
+        Intent patientHomePage = new Intent(this, BasicMapActivity.class);
         patientHomePage.putExtra("JSON", json);
         startActivity(patientHomePage);
     }
