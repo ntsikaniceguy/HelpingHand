b6256a6007a4988d4dcd5bdc544b76f83a3e16a6
Ntsika <thentsikamabe@gmail.com>
2020-06-26 15:52:35 +0200

    Merge branch 'master' of github.com:/ntsikaniceguy/HelpingHand
    # Conflicts:
    #	app/src/main/java/com/example/helpinghand/CreateAccount.java
    #	app/src/main/java/com/example/helpinghand/LoginActivity.java
    #	app/src/main/res/layout/activity_login.xml

---
+++ app/src/main/java/com/example/helpinghand/VolunteerAcceptOrder.java
@@ -0,0 +1,14 @@
+package com.example.helpinghand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VolunteerAcceptOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_accept_order);
+    }
+}
